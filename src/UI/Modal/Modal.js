6a5b0d83c8e09a687b36464a9575f5f6c2aea7de
Syed Shahiq <shahiqurrehman@gmail.com>
2019-08-02 11:48:55 +0500

    Improved performance

--- src/UI/Modal/Modal.js
+++ src/UI/Modal/Modal.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Modal.css'
 import Aux from '../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const Modal = (props) => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.cancelled}></Backdrop>
-    <div className="Modal" style={{
-      transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-      opacity: props.show ? '1' : '0'
-    }}>
-      {props.children}
-    </div>
-  </Aux>
-);
+class Modal extends Component {
+  shouldComponentUpdate (nextProps,nextState) {
+    if(nextProps.show !==this.props.show) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.cancelled}></Backdrop>
+        <div className="Modal" style={{
+          transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: this.props.show ? '1' : '0'
+        }}>
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
 
 export default Modal;
--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 const capitalize = (s) => {
   if (typeof s !== 'string') return ''
   return s.charAt(0).toUpperCase() + s.slice(1)
 }
-const OrderSummary = (props) => {
-  const orderSummary = Object.entries(props.ingredients);
-  const summary = orderSummary.map(ingredient => (
-    <li key={ingredient[0]}>{capitalize(ingredient[0]) + ':' + ingredient[1]}</li>
-  ));
-  return (
-    <Aux>
-      <h1>Your Order</h1>
-      <p>A delicious burger with following ingredients:</p>
-      <ul>
-        {summary}
-      </ul>
-      <p>Current Price: $<strong>{props.price.toFixed(2)}</strong></p>
-      <p>Continue to checkout?</p>
-      <Button clicked={props.puchaseCancelled} class="Danger">Cancel</Button>
-      <Button clicked={props.purchaseContinue} class="Success">Continue</Button>
-    </Aux>
-  );
+class OrderSummary extends Component {
+  render() {
+    const orderSummary = Object.entries(this.props.ingredients);
+    const summary = orderSummary.map(ingredient => (
+      <li key={ingredient[0]}>{capitalize(ingredient[0]) + ':' + ingredient[1]}</li>
+    ));
+    return (
+      <Aux>
+        <h1>Your Order</h1>
+        <p>A delicious burger with following ingredients:</p>
+        <ul>
+          {summary}
+        </ul>
+        <p>Current Price: $<strong>{this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to checkout?</p>
+        <Button clicked={this.props.puchaseCancelled} class="Danger">Cancel</Button>
+        <Button clicked={this.props.purchaseContinue} class="Success">Continue</Button>
+      </Aux>
+    );
+  }
 }
 
 export default OrderSummary;
