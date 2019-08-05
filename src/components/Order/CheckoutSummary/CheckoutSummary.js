1f0fe492cbe7610ab74b7cb51956c0a53ad71dc0
Syed Shahiq <shahiqurrehman@gmail.com>
2019-08-05 17:16:59 +0500

    Click listeners added

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -8,8 +8,8 @@ const checkoutSummary = (props) => {
             <h1>Hope it tastes well!</h1>
             <div style={{ width: '300px', height: '300px', margin: 'auto' }}>
                 <Burger ingredients={props.ingredients} />
-                <Button clicked class="Danger">CANCEL</Button>
-                <Button clicked class="Success">Success</Button>
+                <Button clicked={props.checkoutCancelled} class="Danger">CANCEL</Button>
+                <Button clicked={props.checkoutContinued} class="Success">Success</Button>
             </div>
         </div>
     )
