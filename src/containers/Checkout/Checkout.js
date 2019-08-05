0a00c9f771ce169db935652c78e429231a61d6fc
Syed Shahiq <shahiqurrehman@gmail.com>
2019-08-05 19:13:12 +0500

    Completed Checkout functionality with redirect to home page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import { Route } from 'react-router-dom';
-import ContactData from './CheckoutData/CheckoutData';
+import CheckoutData from './CheckoutData/CheckoutData';
 class Checkout extends Component {
   state = {
-    ingredients: {
-      salad: 1,
-      meat: 1,
-      cheese: 1,
-      bacon: 1
-    }
+    ingredients: null,
+    price: 0
   }
-  componentDidMount() {
+  componentWillMount() {
     const query = new URLSearchParams(this.props.location.search);
     let ingredients = {};
+    let price = 0;
     for (let param of query.entries()) {
-      ingredients[param[0]] = +param[1];
+      if (param[0] === 'price') {
+        price = param[1];
+      } else {
+        ingredients[param[0]] = +param[1];
+      }
     }
-    this.setState({ ingredients: ingredients });
+    this.setState({ ingredients: ingredients, price: price });
   }
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
@@ -29,7 +30,7 @@ class Checkout extends Component {
     return (
       <div>
         <CheckoutSummary ingredients={this.state.ingredients} checkoutCancelled={this.checkoutCancelledHandler} checkoutContinued={this.checkoutContinuedHandler} />
-        <Route path={this.props.match.path + '/contact-data'} component={ContactData}/>
+        <Route path={this.props.match.path + '/contact-data'} render={(props) => <CheckoutData price={this.state.price} ingredients={this.state.ingredients} {...this.props} />} />
       </div>
     )
   }
--- src/containers/Checkout/CheckoutData/CheckoutData.js
+++ src/containers/Checkout/CheckoutData/CheckoutData.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import axios from '../../../axios-orders';
 import Button from '../../../UI/Button/Button';
+import Spinner from '../../../UI/Spinner/Spinner';
 import './CheckoutData.css';
 class CheckoutData extends Component {
   state = {
@@ -8,19 +10,51 @@ class CheckoutData extends Component {
     address: {
       street: '',
       postalCode: ''
+    },
+    loading: false
+  }
+  orderHandler = (event) => {
+    event.preventDefault();
+    this.setState({ loading: true });
+    const order = {
+      ingredients: this.props.ingredients,
+      price: this.props.price,
+      customer: {
+        name: 'Syed Shahiq',
+        address: {
+          street: 'Teststreet 1',
+          zipcode: '12321'
+        },
+        email: 'shahiqurrehman@gmail.com',
+      },
+      deliveryMethod: 'fastest'
     }
+    axios.post('/orders.json', order).then(response => {
+      this.setState({ loading: false });
+      this.props.history.push("/");
+    }).catch(error => {
+      this.setState({ loading: false })
+    })
   }
   render() {
-    return (
-      <div className="ContactData">
+    let form = (
+      <div>
         <h4>Enter Your Contact Data</h4>
         <form>
           <input type="text" name="name" placeholder="Your Name" />
           <input type="email" name="email" placeholder="Your Email" />
           <input type="text" name="street" placeholder="Your Street" />
           <input type="text" name="postalcode" placeholder="Your Postal Code" />
-          <Button class="Success">ORDER NOW</Button>
+          <Button class="Success" clicked={this.orderHandler}>ORDER NOW</Button>
         </form>
+      </div>
+    )
+    if (this.state.loading) {
+      form = <Spinner />
+    }
+    return (
+      <div className="ContactData">
+        {form}
       </div>
     )
   }
