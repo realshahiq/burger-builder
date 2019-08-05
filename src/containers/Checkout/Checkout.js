5d41beb6f4681a192f8a974eea5667ff4efb78eb
Syed Shahiq <shahiqurrehman@gmail.com>
2019-08-05 17:52:14 +0500

    Data passed through query parameters and get data in checkout page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 class Checkout extends Component {
-    state = {
-        ingredients: {
-            salad: 1,
-            meat: 1,
-            cheese: 1,
-            bacon: 1
-        }
+  state = {
+    ingredients: {
+      salad: 1,
+      meat: 1,
+      cheese: 1,
+      bacon: 1
     }
-    checkoutCancelledHandler = () => {
-        this.props.history.goBack();
+  }
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    let ingredients = {};
+    for (let param of query.entries()) {
+      ingredients[param[0]] = +param[1];
     }
-    checkoutContinuedHandler = () => {
-        this.props.history.replace("/checkout/contact-data");
-    }
-    render() {
-        return (
-            <div>
-                <CheckoutSummary ingredients={this.state.ingredients} checkoutCancelled={this.checkoutCancelledHandler} checkoutContinued={this.checkoutContinuedHandler} />
-            </div>
-        )
-    }
+    this.setState({ ingredients: ingredients });
+  }
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  }
+  checkoutContinuedHandler = () => {
+    this.props.history.replace("/checkout/contact-data");
+  }
+  render() {
+    return (
+      <div>
+        <CheckoutSummary ingredients={this.state.ingredients} checkoutCancelled={this.checkoutCancelledHandler} checkoutContinued={this.checkoutContinuedHandler} />
+      </div>
+    )
+  }
 }
 
 export default Checkout;
