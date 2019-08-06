c97095f614a40c35187b5dccb98464bf0e721eb2
Syed Shahiq <shahiqurrehman@gmail.com>
2019-08-06 11:19:16 +0500

    Fetching orders from server

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './Order.css';
 const order = (props) => (
   <div className="Order">
-    <p>Ingredients: Salad(1)</p>
-    <p>Price:<strong>USD 5.45</strong></p>
+    <p>Ingredients:Salad({props.ingredients.salad}),Bacon({props.ingredients.bacon}),Cheese({props.ingredients.cheese}),Meat({props.ingredients.meat})</p>
+    <p>Price:<strong>{props.price}</strong></p>
   </div>
 )
 export default order;
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import WithErrorHandler from '../../hoc/WithErrorHandler/WithErrorHandler';
+import Spinner from '../../UI/Spinner/Spinner';
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true
+  }
+  componentDidMount() {
+    axios.get("/orders.json").then(response => {
+      const fetchOrder = [];
+      for (let key in response.data) {
+        fetchOrder.push(response.data[key]);
+      }
+      this.setState({ orders: fetchOrder });
+      this.setState({ loading: false });
+    }).catch(error => {
+      this.setState({ loading: false });
+    })
+  }
   render() {
+    let orders = <Spinner />
+    if (!this.state.loading)
+       orders = this.state.orders.map((order, index) => {
+        return <Order key={index} ingredients={order.ingredients} price={parseFloat(order.price).toFixed(2)} />
+      })
     return (
-      <div>
-        <Order />
-        <Order />
+      <div style={{padding:"20px"}}>
+        {orders}
       </div>
     )
   }
 }
-export default Orders;
+export default WithErrorHandler(Orders, axios);
